7d20cc37778a21e25dc5cbd43d23558d8096c48e
Lizard <shotup0101@gmail.com>
2020-03-30 09:40:57 +0800

    add info messagebox to operations

--- ftp_interface.py
+++ ftp_interface.py
@@ -2,15 +2,19 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import font
+from tkinter import Canvas
 from ftplib import FTP
 import pathlib
 
 folders = []
 files = []
 filelist = []
+
+# get current working directory
 path = pathlib.Path(__file__).parent.absolute()
 
-# initializr FTP
+# initialize FTP
 ftp = FTP()
 window = tk.Tk()
 
@@ -20,48 +24,60 @@ def download_ftp(name):
     filename = name
     if name in files:
         # 如果有在檔案列表內才下載
-        print(filename)
-        ftp.retrbinary('RETR %s' % filename, open(
+        res = ftp.retrbinary('RETR %s' % filename, open(
             str(path) + '\\' + filename, 'wb').write)
+        if '226' in res:
+            messagebox.showinfo("提示", "已下載檔案")
+        else:
+            messagebox.showinfo("提示", "檔案下載失敗")
 
 
 def upload_ftp():
     filename = filedialog.askopenfilename(initialdir="/", title="Select file")
-    print(filename)
     ftp_file_name = filename.split('/')
-    ftp.storbinary(
+    res = ftp.storbinary(
         "STOR " + ftp_file_name[len(ftp_file_name) - 1], open(filename, 'rb'))
     ftp_refresh()
+    if '226' in res:
+        messagebox.showinfo("提示", "已上傳檔案")
+    else:
+        messagebox.showinfo("提示", "檔案上傳失敗")
 
 
 def del_ftp(name):
+    res = ''
     if name in files:
-        ftp.delete(name)
+        res = ftp.delete(name)
     elif name in folders:
-        ftp.rmd(name)
+        res = ftp.rmd(name)
     ftp_refresh()
+    if '250' in res:
+        messagebox.showinfo('提示', '刪除 ' + name + ' 成功')
+    else:
+        messagebox.showinfo('提示', '刪除 ' + name + ' 失敗')
 
 
 def get_select_item(evt):
     item_name = lb.get(lb.curselection())
-    if item_name in folders:
+    # 如果選的東西是資料夾 進入資料夾
+    if item_name in folders or item_name == '..':
         get_next_dir(item_name)
-    elif item_name == '..':
-        get_next_dir(item_name)
+    elif item_name in files:
+        download_ftp(item_name)
 
 
 def set_path(tk, p_label):
     global path
-    path = filedialog.askdirectory(initialdir="/", title="Select file")
+    new_path = filedialog.askdirectory(initialdir=path, title="Select file")
+    # if new path has been selected
+    if new_path != '':
+        path = new_path
     p_label.configure(text=path)
-    print(path)
 
 
 def search_init(name):
     if search_file(name) == False:
         messagebox.showerror("提示", "找不到檔案")
-    else:
-        messagebox.showinfo("提示", "已下載檔案")
 
 
 def search_file(name):
@@ -100,20 +116,27 @@ right_frame = tk.Frame(window)
 right_frame.pack(side=tk.RIGHT)
 left_frame = tk.Frame(window)
 left_frame.pack(side=tk.LEFT)
+
 d_button = tk.Button(right_frame, text="下載", width=32,
-                     height=2, fg='red', command=lambda: download_ftp(lb.get(lb.curselection())))
+                     height=2, fg='red', command=lambda: download_ftp(lb.get(lb.curselection())),
+                     font=font.Font(size=10))
 u_button = tk.Button(right_frame, text="上傳", width=32,
-                     height=2, fg='red', command=lambda: upload_ftp())
-p_label = tk.Label(right_frame, text=path, width=32, height=2)
-search_entry = tk.Entry(right_frame, text=path, width=32)
-p_button = tk.Button(right_frame, text="設定路徑", width=32,
-                     height=2, fg='red', command=lambda: set_path(tk, p_label))
+                     height=2, fg='red', command=lambda: upload_ftp(), font=font.Font(size=10))
+p_label = tk.Label(right_frame, text=path, width=32,
+                   height=2, font=font.Font(size=10))
+search_entry = tk.Entry(right_frame, text=path,
+                        width=32, font=font.Font(size=10))
+p_button = tk.Button(right_frame, text="設定儲存路徑", width=32,
+                     height=2, fg='red', command=lambda: set_path(tk, p_label), font=font.Font(size=10))
 del_button = tk.Button(right_frame, text="刪除", width=32,
-                       height=2, fg='red', command=lambda: del_ftp(lb.get(lb.curselection())))
+                       height=2, fg='red', command=lambda: del_ftp(lb.get(lb.curselection())),
+                       font=font.Font(size=10))
 new_dir_button = tk.Button(right_frame, text="新增資料夾", width=32,
-                           height=2, fg='red', command=lambda: name_dir())
+                           height=2, fg='red', command=lambda: name_dir(), font=font.Font(size=10))
 search_button = tk.Button(right_frame, text="搜尋", width=32,
-                          height=2, fg='red', command=lambda: search_init(search_entry.get()))
+                          height=2, fg='red', command=lambda: search_init(search_entry.get()),
+                          font=font.Font(size=10))
+
 d_button.pack(side=tk.TOP)
 u_button.pack()
 del_button.pack()
@@ -122,7 +145,7 @@ search_entry.pack(ipady=4)
 search_button.pack()
 p_button.pack()
 p_label.pack()
-lb = tk.Listbox(left_frame, width=60, height=40)
+lb = tk.Listbox(left_frame, width=60, height=40, font=font.Font(size=30))
 lb.bind('<Double-Button>', get_select_item)
 lb.pack()
 
@@ -132,7 +155,7 @@ def ftp_init():
     # and connect to server on 203.75.195.122:21
     # login in as administrator
     # print if fail on initializing
-    ftp.set_debuglevel(2)
+    ftp.set_debuglevel(0)
     # 這樣才可以用中文
     ftp.encoding = 'utf-8'
     ftp.connect("203.75.190.122", 21)
@@ -142,6 +165,7 @@ def ftp_init():
     if "220" not in welcomemsg:
         messagebox.showerror("提示", "無法連線到FTP伺服器")
 
+    lb.insert(0, '..')
     filelist = ftp.nlst()
 
     for dir in filelist:
@@ -190,18 +214,20 @@ def get_next_dir(name):
 def name_dir():
     dialog = tk.Tk()
     dialog.title('新資料夾名稱')
-    dialog.geometry('200x150')
-    n_entry = tk.Entry(dialog, width=20)
-    n_button = tk.Button(dialog, text='新增', height=3, width=10,
-                         command=lambda: make_dir(dialog, n_entry.get()))
-    n_entry.pack(ipady=6)
+    dialog.geometry('250x150')
+    #can = Canvas(dialog, height=75, width=125)
+    #can.place(relx=0.5, rely=0.5, anchor='center')
+    n_entry = tk.Entry(dialog, width=20, font=font.Font(size=30))
+    n_button = tk.Button(dialog, text='新增', height=2, width=10,
+                         command=lambda: make_dir(dialog, n_entry.get()), font=font.Font(size=30))
+    n_entry.pack(pady=20, ipady=6)
     n_button.pack()
     dialog.mainloop()
 
 
 def make_dir(dialog, name):
     dialog.destroy()
-    ftp.mkd(name)
+    res = ftp.mkd(name)
     ftp_refresh()
 
 
